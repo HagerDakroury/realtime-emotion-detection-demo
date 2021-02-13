ae44aa054710718521e79f0fdeaaa312be9e6cd1
HagerDakroury <hagerdakroury@gmail.com>
2021-02-13 18:14:23 +0200

    added img extraction and initial preprocessing

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const video=document.getElementById('wcf')
+const EMOTIONS =  ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"];
+const SIZE= 127;
+const video=document.getElementById('wcf');
 
+let model;
 
 async function setupWCF (){
 
@@ -16,4 +19,30 @@ async function setupWCF (){
     });
 }
 
+/**
+ * captures an image from the webcam feed and preprocesses it
+ * @returns  A tensor object representing the image and ready to pass to model.predict()
+ */
+
+
+function preprocessImg() {
+    return tf.tidy(() => {
+      // reads the image from the video stream and convert it to a tensor object
+      const wcImg = tf.browser.fromPixels(video);
+
+      //resizes the image to to the model's input dimensions
+      const resizedImg = tf.image.resizeBilinear(wcImg, [SIZE, SIZE], true);
+
+      //normalizes the image to [-1,1], originally [0,225]
+      const normalizedImg = resizedImg.toFloat().sub(tf.scalar(1)).div(tf.scalar(127));
+
+      return normalizedImg;
+
+    });
+  }
+
 setupWCF();
+video.addEventListener('loadeddata',function(){
+    console.log(preprocessImg());
+
+});
