e635877ead341748a14fbc111b850423d595820e
HagerDakroury <hagerdakroury@gmail.com>
2021-02-15 11:43:16 +0200

    lighter face tracking but less accurate

--- faceDetector.js
+++ faceDetector.js
@@ -1,8 +1,13 @@
+const DETECT_MODEL_PATH='./public/models/tiny_face_detector/tiny_face_detector_model-weights_manifest.json';
+
 
 async function loadDetection(){
     // Load the model.
-    return model = await faceLandmarksDetection.load(
-      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+
+    return await faceapi.loadTinyFaceDetectorModel(DETECT_MODEL_PATH);
+
+    /*return model = await faceLandmarksDetection.load(
+      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);*/
     
 
 }
@@ -12,13 +17,19 @@ async function loadDetection(){
 
 async function detectFaces(model) {
 
+  const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 256 });
+
+  const predictions = await faceapi.detectAllFaces(document.getElementById('wcf'),options);
+
+
+
     // Pass in an image or video to the model. The model returns an array of
     // bounding boxes, probabilities, and landmarks, one for each detected face.
   
-    const predictions = await model.estimateFaces({
+    /*const predictions = await model.estimateFaces({
       input: document.getElementById("wcf")
     });
-    console.log(predictions);
+    console.log(predictions);*/
       
     
     return predictions;
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const EMOTIONS =  ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"];
-const SIZE= 100;
+const SIZE= 64;
 const video=document.getElementById('wcf');
 const canvas =document.getElementById('overlay');
 
 
-const MODEL_PATH="./public/models/otherModel/model.json";
+const MODEL_PATH="./public/models/emotion/model.json";
 
-let model,predictions;
+let model,emodel,predictions;
 
 
 
 async function renderPredictions(){
   predictions=await detectFaces(model);
+
 
   canvas.width=video.width;
   canvas.height=video.height;
@@ -20,19 +21,32 @@ async function renderPredictions(){
 
   let ctx=context;
   ctx.lineWidth = 4;
-
+  let eImg,img_x,img_y;
 
   for (let i = 0; i < predictions.length; i++) {
-    const start = predictions[i].boundingBox.topLeft;
-    const end = predictions[i].boundingBox.bottomRight;
-    const size = [end[0] - start[0], end[1] - start[1]];
+     img_x = predictions[i].box._x;
+    
+     img_y = predictions[i].box._y;
+    let img_h = predictions[i].box._height;
+    let img_w = predictions[i].box._width;
 
     // Render a rectangle over each detected face.
-    ctx.rect(start[0], start[1], size[0], size[1]);
+    ctx.rect(img_x, img_y, img_w, img_h);
 
+    eImg=ctx.getImageData(img_x, img_y, img_w, img_h);
 
   
 }
+
+let image=await preprocessImg(eImg);
+
+
+const eprediction=emodel.predict(image);
+
+
+let index=eprediction.argMax(1).dataSync()[0];
+let label =EMOTIONS[index];
+ctx.fillText(label, img_x,img_y);
 
   
   ctx.strokeStyle = "red";    
@@ -69,21 +83,23 @@ async function setupWCF (){
  */
 
 
-async function preprocessImg() {
+async function preprocessImg(img) {
+
     return tf.tidy(() => {
       // reads the image from the video stream and convert it to a tensor object
-      const wcImg = tf.browser.fromPixels(video).toFloat();
+      const wcImg = tf.browser.fromPixels(img).toFloat();
 
       //resizes the image to to the model's input dimensions
-      const resizedImg = tf.image.resizeBilinear(wcImg, [SIZE, SIZE], true);
+      const resizedImg = wcImg.resizeBilinear( [SIZE, SIZE]);
 
       //normalizes the image to [-1,1], originally [0,225]
       const normalizedImg = resizedImg.toFloat().sub(tf.scalar(1)).div(tf.scalar(127));
 
-      //const greyImg=normalizedImg.mean(2).expandDims(2);
+      const greyImg=normalizedImg.mean(2).expandDims(2);
 
-      const batchedImg = normalizedImg.expandDims(0);
-      
+      const batchedImg = greyImg.expandDims(0);
+
+
       return batchedImg;
 
     });
@@ -118,6 +134,8 @@ setupWCF();
 
 video.onplay= async function (){
   model= await loadDetection();
+  emodel=await tf.loadLayersModel(MODEL_PATH);
+
   setTimeout(renderPredictions , 300);
 
 }
